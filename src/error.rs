7b52785f94904882c4f93057bd755b856e047c4a
Cobrand <cobrandw@gmail.com>
2017-01-26 00:58:47 +0100

    Fixed ugly shit

--- src/error.rs
+++ src/error.rs
@@ -1,32 +1,10 @@
-use iron::{status,IronError,Response};
+use iron::{status,IronError,IronResult,Response};
 
 pub type StdResult<T,E> = ::std::result::Result<T,E>;
 
-macro_rules! chatbix_try {
-    ($expr:expr) => (
-        match $expr {
-            Ok(val) => val,
-            Err(error) => return match error {
-                Error(ErrorKind::ChronoParseError(parse_error),_) =>
-                    Ok(Response::with((format!("{}",parse_error),status::UnprocessableEntity))),
-                Error(ErrorKind::InvalidCredentials, _) =>
-                    Ok(Response::with(("invalid username or password",status::UnprocessableEntity))),
-                Error(ErrorKind::InvalidAuthKey, _) =>
-                    Ok(Response::with(("invalid auth_key",status::Unauthorized))),
-                Error(ErrorKind::MissingParameter(missing_param_name), _) =>
-                    Ok(Response::with((format!("missing parameter `{}`",missing_param_name),status::UnprocessableEntity))),
-                e => Err(IronError::new(e, status::InternalServerError)),
-            }
-        }
-    )
-}
-
-/*
-pub fn iron_result(result:Result<Response>) -> ::iron::IronResult<Response> {
-    result.or_else(|error: Error| {
-        // IronError are meant to be logged, they are typically 5XX errors
-        // Errors converted as Responses can be safely ignored and must not be
-        // logged, they are typically 4XX errors
+impl Into<IronResult<Response>> for Error {
+    fn into(self) -> IronResult<Response> {
+        let error = self;
         match error {
             Error(ErrorKind::ChronoParseError(parse_error),_) =>
                 Ok(Response::with((format!("{}",parse_error),status::UnprocessableEntity))),
@@ -38,9 +16,17 @@ pub fn iron_result(result:Result<Response>) -> ::iron::IronResult<Response> {
                 Ok(Response::with((format!("missing parameter `{}`",missing_param_name),status::UnprocessableEntity))),
             e => Err(IronError::new(e, status::InternalServerError)),
         }
-    })
+    }
 }
-*/
+
+macro_rules! chatbix_try {
+    ($expr:expr) => (
+        match $expr {
+            Ok(val) => val,
+            Err(error) => return error.into()
+        }
+    )
+}
 
 error_chain! {
     errors {
